d334b5591faa6695d7a9f4327f51e5c089e3bda1
Madhva Malhotra <madhav.malhotra003@gmail.com>
2024-09-06 16:14:33 -0400

    parameterised dump file to include local array registers

--- test/dump_pe.v
+++ test/dump_pe.v
@@ -1,7 +1,16 @@
-module dump();
+module dump#(
+    // Parameters for size of local register file
+    parameter REG_SIZE = 4
+)();
     initial begin
+        // This syntax won't dump any array variables
         $dumpfile ("./test/pe.vcd");
         $dumpvars (0, pe);
+
+        // So we manually dump array variables
+        for (int i=0; i<REG_SIZE; i=i+1) begin
+            $dumpvars (0, pe.regfile[i]);
+        end
         #1;
     end
 endmodule
